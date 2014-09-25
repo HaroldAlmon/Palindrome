33b8e582e2e19be17ce698d1e5fa6193b25e0e28
haroldalmon <haroldalmon@hotmail.com>
2014-09-25 18:01:39 -0500

    initial version

---
+++ Palindrome.js
@@ -0,0 +1,43 @@
+function evalText() {
+	var isNot = "";
+	var text = text = document.getElementById("inputField").value;
+
+	if(text === '') {
+		document.getElementById("result").innerHTML = "Please enter some text in the field above.";
+		document.getElementById("inputField").focus();
+		return
+	}
+	
+	if (isPal(text) !== true) {
+		isNot = " not";
+	}
+	document.getElementById("result").innerHTML = "<span style='font-weight:bold'>" + text + "</span> is" + isNot + " a palindrome.";
+	document.getElementById("inputField").focus();
+	document.getElementById("inputField").value = '';
+	return;
+	
+	function isPal(text) {
+		var i, j; // Used as left and right indices in the string.
+		var result = true;
+
+		// Treat upper and lowercase as equal...
+		text = text.toUpperCase();
+		
+		// Strip out non-alpha characters...
+		text = text.replace(/[^A-Za-z0-9]/g, '');
+		
+		i = 0;
+		j = text.length - 1;
+
+		// Move towards the center of the string from the start and end of
+		// the string...
+		for (; i < j; i++, j--) {
+			if (text.charAt(i) !== text.charAt(j)) {
+				// Okay, chars fo not match, it's not a palindrome...
+				result = false;
+				break;
+			}
+		}
+		return result;
+	}
+}
